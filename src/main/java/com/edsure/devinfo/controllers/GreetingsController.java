b73a599f9e0ec299d4441eb02789d18436d3e06f
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-11-30 11:09:25 +0000

    END-POINT Emplementados

--- src/main/java/com/edsure/devinfo/controllers/GreetingsController.java
+++ src/main/java/com/edsure/devinfo/controllers/GreetingsController.java
@@ -25,10 +25,8 @@ public class GreetingsController {
     @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String retornaOlaMundo(@PathVariable String nome) {
-
         Usuario usuario = new Usuario();
         usuario.setNome(nome);
-
         usuarioRepository.save(usuario);//gravar no banco de dados
         return "Ola mundo " + nome;
     }
@@ -38,5 +36,50 @@ public class GreetingsController {
     public ResponseEntity<List<Usuario>> listaUsuario() {
         List<Usuario> usuarios = usuarioRepository.findAll();
         return new ResponseEntity<>(usuarios, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "salvar")
+    @ResponseBody
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){
+        Usuario user = usuarioRepository.save(usuario);
+        return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+
+    }
+
+
+    @DeleteMapping(value = "delete")
+    @ResponseBody
+    public ResponseEntity<String> delete(@RequestParam Long iduser){
+        usuarioRepository.deleteById(iduser);
+        return new ResponseEntity<String>("user deletado com sucesso", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "buscaruserid")
+    @ResponseBody
+    public ResponseEntity<Usuario> buscaruserid(@RequestParam(name = "iduser") Long iduser){
+        Usuario usuario = usuarioRepository.findById(iduser).get();
+        return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+    }
+
+
+    @PutMapping(value = "atualizar")
+    @ResponseBody
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario){
+        if(usuario.getId()== null) {
+            return new ResponseEntity<String>("Id não foi informado para atualização.", HttpStatus.OK);
+        }
+
+        Usuario user = usuarioRepository.saveAndFlush(usuario);
+
+        return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+
+    }
+
+    @GetMapping(value = "buscarPorNome")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> buscarPorNome(@RequestParam(name = "name") String name){
+        List <Usuario> usuario = usuarioRepository.buscarPorNome(name.trim().toUpperCase());
+        return new ResponseEntity<List<Usuario>>(usuario, HttpStatus.OK);
+
     }
 }
--- src/main/java/com/edsure/devinfo/repository/UsuarioRepository.java
+++ src/main/java/com/edsure/devinfo/repository/UsuarioRepository.java
@@ -2,8 +2,14 @@ package com.edsure.devinfo.repository;
 
 import com.edsure.devinfo.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    @Query(value = "select u from Usuario u where upper(trim(u.nome)) like %?1%")
+    List<Usuario> buscarPorNome(String name);
 }
